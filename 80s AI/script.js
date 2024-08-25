e8ca33bfe1a55d2ed39e82752ec125402f465802
Nathan Grossmann <94024721+nategr54@users.noreply.github.com>
2024-08-25 15:03:28 -0600

    Add files via upload

---
+++ 80s AI/script.js
@@ -0,0 +1,13 @@
+document.getElementById("generate-advice").addEventListener("click", function() {
+
+    fetch('https://api.adviceslip.com/advice')
+    .then(response => response.json())
+    .then(data => {
+        const adviceText = data.slip.advice.toUpperCase()
+        document.getElementById('advice-text').innerText = adviceText
+    })
+    .catch(error => {
+        document.getElementById('advice-text').innerText = "ERROR FETCHING ADVICE..."
+    })
+})
+
